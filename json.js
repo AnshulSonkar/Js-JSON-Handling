95b2d99873c9d66c7a72eefa84d45280d779e774
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-07-03 19:42:46 +0530

    commit

--- json.js
+++ json.js
@@ -34,25 +34,28 @@
 // // }
 
 
-let input = document.getElementById("nameInput");
-let button = document.getElementById("saveBtn");
-let output = document.getElementById("output");
 
-button.addEventListener("click", function () {
 
-  let user = {
-    name: input.value
-  };
+// new main code
+// let input = document.getElementById("nameInput");
+// let button = document.getElementById("saveBtn");
+// let output = document.getElementById("output");
 
-  localStorage.setItem("user", JSON.stringify(user));
+// button.addEventListener("click", function () {
+
+//   let user = {
+//     name: input.value
+//   };
+
 //   localStorage.setItem("user", JSON.stringify(user));
+// //   localStorage.setItem("user", JSON.stringify(user));
 
-  output.textContent = "Saved!";
-});
+//   output.textContent = "Saved!";
+// });
 
-let savedUser = JSON.parse(localStorage.getItem("user"));
 // let savedUser = JSON.parse(localStorage.getItem("user"));
+// // let savedUser = JSON.parse(localStorage.getItem("user"));
 
-if (savedUser) {
-  output.textContent = "Stored Name: " + savedUser.name;
-}
+// if (savedUser) {
+//   output.textContent = "Stored Name: " + savedUser.name;
+// }
